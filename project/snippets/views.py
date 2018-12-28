0f4225ff16988722f0dc2f1103a8540ebc104a90
uj <ubell91@gmail.com>
2018-12-28 20:28:43 +0900

    Tutorial 2. decorator적용, url패턴에 접미사로 포맷형식지정

--- project/snippets/views.py
+++ project/snippets/views.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 #
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+#
 from .models import Snippet
 from .serializers import SnippetSerializer
 
@@ -13,13 +15,14 @@ class JSONResponse(HttpResponse):
     콘텐츠(data)를 JSON으로 변환한 후 , HttpResponse 형태로 반환함.
     """
     def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
+        content = JSONRenderer().render(data) # Renderer 객체 리턴받고, 리턴받은 Renderer의 함수 render()실행
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content,**kwargs)
 
 
+@api_view(['GET', 'POST'])
 @csrf_exempt
-def snippet_list(request) -> JSONResponse:
+def snippet_list(request, format=None) -> JSONResponse:
     """
     :param request: request 객체.
     :return: JSONResponse
@@ -38,8 +41,9 @@ def snippet_list(request) -> JSONResponse:
         return JSONResponse(serializer.errors, status=400)
 
 
+@api_view(['GET', 'PUT', 'DELETE']) # request method가 get, put, delete 일 때에만 해당 함수에 접근 가능하도록함.
 @csrf_exempt
-def snippet_detail(request, pk) -> JSONResponse:
+def snippet_detail(request, pk, format=None) -> JSONResponse:
     """
     코드 조각 조회, 업데이트, 삭제
     :param request: request객체
