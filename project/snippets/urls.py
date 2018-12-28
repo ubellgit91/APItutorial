d3d6181777e59e0df374c00b8fa14ddb1c42fec6
uj <ubell91@gmail.com>
2018-12-28 20:28:43 +0900

    Tutorial 2. CBV 기본형태.

--- project/snippets/urls.py
+++ project/snippets/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
+#
+from rest_framework.urlpatterns import format_suffix_patterns
+#
 from . import views
 
 app_name = 'snippets'
@@ -6,3 +9,7 @@ urlpatterns = [
     url(r'^$', views.snippet_list),
     url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+# 접미사(suffix)로 format형식 지정해주기.
+# 해당 함수를 사용하면 api를 호출할 때, 포멧접미사를 붙여서 포맷을 지정할 수 있음
+urlpatterns = format_suffix_patterns(urlpatterns)
--- project/snippets/views.py
+++ project/snippets/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 #
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+#
 from .models import Snippet
 from .serializers import SnippetSerializer
 
@@ -13,13 +18,63 @@ class JSONResponse(HttpResponse):
     콘텐츠(data)를 JSON으로 변환한 후 , HttpResponse 형태로 반환함.
     """
     def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
+        content = JSONRenderer().render(data) # Renderer 객체 리턴받고, 리턴받은 Renderer의 함수 render()실행
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content,**kwargs)
 
+############## CBV #################
+class SnippetList(APIView): # rest_framework의 APIView를 쓰는 걸 기억해라.
+    """
+    데이터를 모두 보여주거나 새 데이터를 삽입.
+    """
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
+    """
+    데이터 하나 조회, 수정, 삭제
+    """
+    def get_object(self, pk): # pk로 데이터 객체 하나를 get해오기 위한 함수.
+        try:
+            Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404 # 데이터객체가 존재하지않으면 404에러 발생.
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+############## FBV ################
+@api_view(['GET', 'POST'])
 @csrf_exempt
-def snippet_list(request) -> JSONResponse:
+def snippet_list(request, format=None) -> JSONResponse:
     """
     :param request: request 객체.
     :return: JSONResponse
@@ -38,8 +93,9 @@ def snippet_list(request) -> JSONResponse:
         return JSONResponse(serializer.errors, status=400)
 
 
+@api_view(['GET', 'PUT', 'DELETE']) # request method가 get, put, delete 일 때에만 해당 함수에 접근 가능하도록함.
 @csrf_exempt
-def snippet_detail(request, pk) -> JSONResponse:
+def snippet_detail(request, pk, format=None) -> JSONResponse:
     """
     코드 조각 조회, 업데이트, 삭제
     :param request: request객체
